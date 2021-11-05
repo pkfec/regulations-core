427e354379e7d3f14700bf7464b3790e112e7160
Priyanka Kasireddy <pkasireddy@fec.gov>
2021-11-05 14:21:36 -0400

    Bump django-mptt to v0.13.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'cached_property',
         'django~=2.2.11',
-        'django-mptt~=0.11.0',
+        'django-mptt~=0.13.4',
         'jsonschema',
         'six',
         'webargs',
